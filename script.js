c6a4d1bc4f5df27a7b3cabdbbf1affdb7a0c14c6
Vishnu P <vinventor1999@gmail.com>
2024-04-24 23:23:55 +0530

    Animations

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+var hoverableDiv = document.getElementById("about");
+    
+hoverableDiv.addEventListener("touchstart", function() {
+  this.classList.add("touch");
+});
+
+hoverableDiv.addEventListener("touchend", function() {
+  this.classList.remove("touch");
+});
